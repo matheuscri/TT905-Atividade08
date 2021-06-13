5686af6bbde2682c5c1be34dcb066d9acb14c6e8
matheuscri <80854803+matheuscri@users.noreply.github.com>
2021-06-13 18:36:37 -0300

    asdf

--- function.js
+++ function.js
@@ -1,8 +1,11 @@
+// link geral do programa
+let url = "https://tt905-prog-web-matheus-cri.herokuapp.com/"
+
 /********************************************************************** 
 *******************************Cantores********************************
 ************************************************************************/
 
-// let url = "https://tt905-prog-web-matheus-cri.herokuapp.com/cantores/"
+//let url = "https://tt905-prog-web-matheus-cri.herokuapp.com/cantores/"
 
 async function callFetchWithGet(){
     let headers = new Headers();
@@ -12,7 +15,7 @@ async function callFetchWithGet(){
         headers: headers
     }
     const output = document.getElementById("json");
-    const response = await fetch(url, options);
+    const response = await fetch(`${url}${'cantores/'}`, options);
 
     if (response.status >= 200 && response.status <= 300){
         console.log("Funcionou/GET");
@@ -34,7 +37,8 @@ async function callFetchWithPost(cantor){
             'Cantor' : cantor
         })
     }
-    await fetch(url, options);
+    //await fetch(url, options);
+    await fetch(`${url}${'cantores/'}`,options);
 }
 
 async function callFetchWithPut(id, novaMensagem){
@@ -49,7 +53,7 @@ async function callFetchWithPut(id, novaMensagem){
             'Cantor' : novaMensagem
         })
     }
-    await fetch(`${url}${id}`, options);
+    await fetch(`${url}${'cantores/'}${id}`, options);
 }
 
 async function callFetchWithDelete(id){
@@ -61,7 +65,8 @@ async function callFetchWithDelete(id){
             'content-type' : 'application/json' 
         }
     }
-    await fetch(`${url}${id}`, options);
+   // await fetch(`${url}${id}`, options);
+   await fetch(`${url}${'cantores/'}${id}`, options);
 }
 
 /*
@@ -95,7 +100,7 @@ function submitDelete(){
 *******************Soudtrack: Tony Hawk********************************
 ************************************************************************/
 
-let url = "https://tt905-prog-web-matheus-cri.herokuapp.com/soundtrack/Tony_Hawk_American_Wasteland/"
+//let url = "https://tt905-prog-web-matheus-cri.herokuapp.com/soundtrack/Tony_Hawk_American_Wasteland/"
 
 
 async function callFetchWithGets1(){
@@ -106,7 +111,7 @@ async function callFetchWithGets1(){
         headers: headers
     }
     const output = document.getElementById("json1");
-    const response = await fetch(url, options);
+    const response = await fetch(`${url}${'soundtrack/Tony_Hawk_American_Wasteland/'}`, options);
 
     if (response.status >= 200 && response.status <= 300){
         console.log("Funcionou/GET");
@@ -121,14 +126,15 @@ async function callFetchWithPosts1(musica){
         method : 'POST',
         mode: 'cors',
         headers: { //entender isso
-            'Accept' : 'application/json1',
-            'content-type' : 'application/json1'
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'
         },
         body :JSON.stringify({
             'Music' : musica
         })//até isso
     }
-    await fetch(url, options);
+    //await fetch(url, options);
+    await fetch(`${url}${'soundtrack/Tony_Hawk_American_Wasteland/'}`, options);
 }
 
 async function callFetchWithPuts1(id, novaMensagem){
@@ -136,14 +142,15 @@ async function callFetchWithPuts1(id, novaMensagem){
         method : 'PUT',
         mode: 'cors',
         headers: {
-            'Accept' : 'application/json1',
-            'content-type' : 'application/json1'            
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'            
         }, 
         body :JSON.stringify({
             'Music' : novaMensagem
         })
     }
-    await fetch(`${url}${id}`, options);
+    //await fetch(`${url}${id}`, options);
+    await fetch(`${url}${'soundtrack/Tony_Hawk_American_Wasteland/'}${id}`, options);
 }
 
 async function callFetchWithDeletes1(id){
@@ -151,12 +158,14 @@ async function callFetchWithDeletes1(id){
         method : 'DELETE',
         mode: 'cors',
         headers: {
-            'Accept' : 'application/json1',
-            'content-type' : 'application/json1' 
+            'Accept' : 'application/json',
+            'content-type' : 'application/json' 
         }
     }
-    await fetch(`${url}${id}`, options);
+    //await fetch(`${url}${id}`, options);
+    await fetch(`${url}${'soundtrack/Tony_Hawk_American_Wasteland/'}${id}`, options);
 }
+
 
 /*
     Formulários
@@ -184,4 +193,201 @@ function submitDeletes1(){
     const id = form["id"].value;  
     callFetchWithDeletes1(id);
     return false; // Evitar o reload da tela.
+}
+/********************************************************************** 
+*******************Soudtrack: Madden 10********************************
+************************************************************************/
+
+//let url = "https://tt905-prog-web-matheus-cri.herokuapp.com/soundtrack/Madden_NFL_10/"
+
+async function callFetchWithGets2(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+    const output = document.getElementById("json2");
+    const response = await fetch(`${url}${'soundtrack/Madden_NFL_10/'}`, options);
+
+    if (response.status >= 200 && response.status <= 300){
+        console.log("Funcionou/GET");
+        output.innerHTML = await response.text();
+    } else {
+        console.log("Deu errado");
+    }
+}
+
+async function callFetchWithPosts2(musica){
+    const options = {
+        method : 'POST',
+        mode: 'cors',
+        headers: { //entender isso
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'
+        },
+        body :JSON.stringify({
+            'Music' : musica
+        })//até isso
+    }
+    //await fetch(url, options);
+    await fetch(`${url}${'soundtrack/Madden_NFL_10/'}`, options);
+}
+
+async function callFetchWithPuts2(id, novaMensagem){
+    const options = {
+        method : 'PUT',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'            
+        }, 
+        body :JSON.stringify({
+            'Music' : novaMensagem
+        })
+    }
+    //await fetch(`${url}${id}`, options);
+    await fetch(`${url}${'soundtrack/Madden_NFL_10/'}${id}`, options);
+}
+
+async function callFetchWithDeletes2(id){
+    const options = {
+        method : 'DELETE',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json' 
+        }
+    }
+    //await fetch(`${url}${id}`, options);
+    await fetch(`${url}${'soundtrack/Madden_NFL_10/'}${id}`, options);
+}
+
+/*
+    Formulários
+*/
+
+function submitPosts2(){
+    console.log("entrei na função");
+    
+    const form = document.forms['postForm'];    
+    const mensagem = form["mensagem"].value;
+    callFetchWithPosts2(mensagem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitPuts2(){
+    const form = document.forms['putForm'];  
+    const id = form["id"].value;  
+    const mensagem = form["mensagem"].value;
+    callFetchWithPuts2(id, mensagem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitDeletes2(){
+    const form = document.forms['deleteForm'];  
+    const id = form["id"].value;  
+    callFetchWithDeletes2(id);
+    return false; // Evitar o reload da tela.
+}
+
+/********************************************************************** 
+*******************Soudtrack: Guitar Hero********************************
+************************************************************************/
+
+//let url = "https://tt905-prog-web-matheus-cri.herokuapp.com/soundtrack/Guitar_hero/"
+
+async function callFetchWithGets3(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+    const output = document.getElementById("json3");
+    const response =  await fetch(`${url}${'soundtrack/Guitar_hero/'}`, options);
+
+    if (response.status >= 200 && response.status <= 300){
+        console.log("Funcionou/GET");
+        output.innerHTML = await response.text();
+    } else {
+        console.log("Deu errado");
+    }
+}
+
+async function callFetchWithPosts3(musica){
+    const options = {
+        method : 'POST',
+        mode: 'cors',
+        headers: { //entender isso
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'
+        },
+        body :JSON.stringify({
+            'Music' : musica
+        })//até isso
+    }
+    //await fetch(url, options);
+    await fetch(`${url}${'soundtrack/Guitar_hero/'}`, options);
+}
+
+async function callFetchWithPuts3(id, novaMensagem){
+    const options = {
+        method : 'PUT',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'            
+        }, 
+        body :JSON.stringify({
+            'Music' : novaMensagem
+        })
+    }
+    //await fetch(`${url}${id}`, options);
+    await fetch(`${url}${'soundtrack/Guitar_hero/'}${id}`, options);
+}
+
+async function callFetchWithDeletes3(id){
+    const options = {
+        method : 'DELETE',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json' 
+        }
+    }
+    //await fetch(`${url}${id}`, options);
+    await fetch(`${url}${'soundtrack/Guitar_hero/'}${id}`, options);
+}
+/*
+    Formulários
+*/
+
+function submitPosts3(){
+    console.log("entrei na função");
+    
+    const form = document.forms['postForm'];    
+    const mensagem = form["mensagem"].value;
+    callFetchWithPosts3(mensagem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitPuts3(){
+    const form = document.forms['putForm'];  
+    const id = form["id"].value;  
+    const mensagem = form["mensagem"].value;
+    callFetchWithPuts3(id, mensagem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitDeletes3(){
+    const form = document.forms['deleteForm'];  
+    const id = form["id"].value;  
+    callFetchWithDeletes3(id);
+    return false; // Evitar o reload da tela.
+}
+
+function submitGet(){
+    callFetchWithGet();
+    return false;
 }
